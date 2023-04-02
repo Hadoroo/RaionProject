62da184b743c25bca2e0a5465396715dbbc6af4e
Hadoroo <128714895+Hadoroo@users.noreply.github.com>
2023-04-02 18:25:06 +0700

    update 2/4/23

--- Assets/Scenes/SceneController.cs
+++ Assets/Scenes/SceneController.cs
@@ -7,18 +7,18 @@ public class SceneController : MonoBehaviour {
 
     public string scene;
 
-    GameObject player;
-
-    void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player");
-    }
-
     private void OnTriggerEnter2D(Collider2D colliderObject)
     {
         SceneManager.LoadScene(scene);
-        DontDestroyOnLoad(colliderObject);
-        Destroy(player);
+    }
 
+    public void play()
+    {
+        SceneManager.LoadScene("lvl1");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }
